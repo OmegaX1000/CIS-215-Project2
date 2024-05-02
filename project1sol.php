96b7d2a78939d9a09553d62818380f288b9e04b5
Matthew Rada <Matthewrada.02@gmail.com>
2024-05-01 22:01:35 -0400

    Feat: Tooltips

--- project1sol.php
+++ project1sol.php
@@ -13,30 +13,30 @@
         <fieldset>
 
         <label>Enter your email: </label>
-        <input type="email" name="email-name" id="email-id" required>
+        <input type="email" name="email-name" id="email-id" title="Enter Your Email Here... (ex. student01@college.edu)" required>
 
         <label>Enter your password: </label>
-        <input type="password" name="pw-name" id="pw-id" required>
+        <input type="password" name="pw-name" id="pw-id" title="Enter your school account password here." required>
 
         </fieldset>
 
         <div id = "age-field">
-            <label>What age are you? </label>
+            <label title="Input your age here. Select Betweeen a range.">What age are you? </label>
             <div>
-                <label> <input type="radio" name="age" id="age-0" value="0" required>
+                <label title="Between 0 and 12 years old"> <input type="radio" name="age" id="age-0" value="0" required>
                 0-12 </label>
             </div>
             <?php
 
             for($i=13;$i<65;$i=$i + 5){
                 $j = $i + 4;
-                print("<div><label><input type='radio' name='age' id='age-$i'value='$i'>
+                print("<div><label title='Between $i and $j years old'><input type='radio' name='age' id='age-$i'value='$i'>
                 $i-$j </label></div>");
             }
 
             ?>
             <div>
-                <label> <input type="radio" name="age" id="age-68" value="68">
+                <label title="68 years or older"> <input type="radio" name="age" id="age-68" value="68">
                 68+ </label>
             </div>
         </div>
@@ -44,7 +44,7 @@
         <br>
     
         <div>
-        <select name="gender" id="gender">
+        <select name="gender" id="gender" title="Choose your gender Identity, if you don't want to say then you can say 'Other'">
             <option value="">--Please select your gender--</option>
             <option value="ma">Male</option>
             <option value="fe">Female</option>
@@ -56,7 +56,7 @@
         </div>
 
         <div name = "version-field">
-            <label> What version of PHP do you use? (only include the main version number) <input type="number" name="version" id="version" min="1", max="9" required> </label>
+            <label> What version of PHP do you use? (only include the main version number) <input type="number" name="version" id="version" min="1", max="9" title="Select what Major version of PHP you use (1 to 9)"required> </label>
         </div>
 
         <div>
@@ -64,7 +64,7 @@
                 Please answer in 120 characters or fewer.
             </div>
             <label> What is your favorite part of PHP?     
-            <input type=text name="favorite" id="favorite" required></label>
+            <input type=text name="favorite" id="favorite" title="Within 120 Characters describe what you like about using PHP."required></label>
             <label> Character Count: <span id = "CharCount">120</span></label>
         </div>
 
